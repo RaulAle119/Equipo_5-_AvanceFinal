dff82c2c0060802cd00bd2765a1f4111da57e28b
RaulAle119 <raul.gonzalezsvr@uanl.edu.mx>
2024-10-27 16:33:29 -0600

    test
    Se hizo un script para comprobar el sistema operativo en donde se encuentra el usuario al ejecutar el script

--- AvFinal.py
+++ AvFinal.py
@@ -1,3 +1,14 @@
 import platform
 
-print(platform.system())
+def analyze_system():
+    system = platform.system()
+
+    if system == "Windows":
+        print("Estas en Windows")
+    elif system == "Linux":
+        print("Estas en Linux")
+    else:
+        print(f"El sistema operativo es: {system}")
+
+if __name__ == "__main__":
+    analyze_system()
