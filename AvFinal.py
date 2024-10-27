341e657d4a55fbe656c1450902f4e3b6177da812
RaulAle119 <raul.gonzalezsvr@uanl.edu.mx>
2024-10-27 17:48:09 -0600

    add
    se empezo a  impotar los modulos de powershell

--- AvFinal.py
+++ AvFinal.py
@@ -1,10 +1,22 @@
 import platform
+import subprocess
+
+def importar_modulo_powershell(modulo):
+    try:
+        # Ejecutar comando de PowerShell para importar el módulo
+        comando = "Import-Module 'C:\\Users\\raulg\\Documents\\GitHub\\Equipo_5-_AvanceFinal\\Show-Resources.psm1'"
+
+        subprocess.run(["powershell", "-Command", comando], check=True)
+        print(f"Modulo '{modulo}' importado correctamente.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error al importar el modulo '{modulo}': {e}")
 
 def analyze_system():
     system = platform.system()
 
     if system == "Windows":
         print("Estas en Windows")
+        importar_modulo_powershell('Show-Resources')
     elif system == "Linux":
         print("Estas en Linux")
     else:
